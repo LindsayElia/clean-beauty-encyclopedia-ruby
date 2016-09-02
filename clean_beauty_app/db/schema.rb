d4caf8e695ecc9a4d7b29b13475b6ec53957350b
LindsayElia <lindsay.elia@gmail.com>
2016-09-02 12:58:47 -0700

    change indentation spacing

--- clean_beauty_app/db/schema.rb
+++ clean_beauty_app/db/schema.rb
@@ -13,59 +13,59 @@
 
 ActiveRecord::Schema.define(version: 20160826214045) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+	# These are extensions that must be enabled in order to support this database
+	enable_extension "plpgsql"
 
-  create_table "brands", force: :cascade do |t|
-    t.text     "name"
-    t.text     "site_url"
-    t.text     "hq_country"
-    t.text     "hq_region"
-    t.text     "hq_postal_code"
-    t.integer  "year_founded"
-    t.boolean  "is_vegan"
-    t.boolean  "contains_vegan"
-    t.boolean  "is_gluten_free"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
+	create_table "brands", force: :cascade do |t|
+		t.text     "name"
+		t.text     "site_url"
+		t.text     "hq_country"
+		t.text     "hq_region"
+		t.text     "hq_postal_code"
+		t.integer  "year_founded"
+		t.boolean  "is_vegan"
+		t.boolean  "contains_vegan"
+		t.boolean  "is_gluten_free"
+		t.datetime "created_at",     null: false
+		t.datetime "updated_at",     null: false
+	end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.text     "name"
-    t.text     "alternate_names"
-    t.boolean  "is_animal_derived"
-    t.boolean  "is_organic"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-  end
+	create_table "ingredients", force: :cascade do |t|
+		t.text     "name"
+		t.text     "alternate_names"
+		t.boolean  "is_animal_derived"
+		t.boolean  "is_organic"
+		t.datetime "created_at",        null: false
+		t.datetime "updated_at",        null: false
+	end
 
-  create_table "product_ingredients", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "ingredient_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
+	create_table "product_ingredients", force: :cascade do |t|
+		t.integer  "product_id"
+		t.integer  "ingredient_id"
+		t.datetime "created_at",    null: false
+		t.datetime "updated_at",    null: false
+	end
 
-  add_index "product_ingredients", ["ingredient_id"], name: "index_product_ingredients_on_ingredient_id", using: :btree
-  add_index "product_ingredients", ["product_id"], name: "index_product_ingredients_on_product_id", using: :btree
+	add_index "product_ingredients", ["ingredient_id"], name: "index_product_ingredients_on_ingredient_id", using: :btree
+	add_index "product_ingredients", ["product_id"], name: "index_product_ingredients_on_product_id", using: :btree
 
-  create_table "products", force: :cascade do |t|
-    t.text     "name"
-    t.text     "category"
-    t.text     "application_type"
-    t.decimal  "price"
-    t.text     "currency"
-    t.text     "size"
-    t.text     "ingredients_grouping"
-    t.text     "image_url"
-    t.text     "image_alt"
-    t.integer  "brand_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
+	create_table "products", force: :cascade do |t|
+		t.text     "name"
+		t.text     "category"
+		t.text     "application_type"
+		t.decimal  "price"
+		t.text     "currency"
+		t.text     "size"
+		t.text     "ingredients_grouping"
+		t.text     "image_url"
+		t.text     "image_alt"
+		t.integer  "brand_id"
+		t.datetime "created_at",           null: false
+		t.datetime "updated_at",           null: false
+	end
 
-  add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
+	add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
 
-  add_foreign_key "product_ingredients", "ingredients"
-  add_foreign_key "product_ingredients", "products"
+	add_foreign_key "product_ingredients", "ingredients"
+	add_foreign_key "product_ingredients", "products"
 end
