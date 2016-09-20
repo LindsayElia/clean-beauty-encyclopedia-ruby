92971dc39163c8fbc0c4465eb573724e840c9ba6
LindsayElia <lindsay.elia@gmail.com>
2016-09-20 13:10:04 -0700

    update routes and add comments for reference

--- clean_beauty_app/config/routes.rb
+++ clean_beauty_app/config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
     
     root 'statics#index'
+    get '/statics', to: 'statics#index'
 
-    get 'products/index'
+    get '/brands', to: 'brands#index'
+    get '/brands/:id', to: 'brands#show', as: 'brands_show'	# this should go after create, new, edit if I add routes for those later
 
-    get 'products/show'
-
-    get 'brands/index'
-
-    get 'brands/show'
-
-    get 'statics/index'
+    get '/products', to: 'products#index'
+    get '/products/:id', to: 'products#show', as: 'products_show'	# same comment ^^
 
 end
 
-#         Prefix Verb URI Pattern               Controller#Action
-#           root GET  /                         statics#index
-# products_index GET  /products/index(.:format) products#index
-#  products_show GET  /products/show(.:format)  products#show
-#   brands_index GET  /brands/index(.:format)   brands#index
-#    brands_show GET  /brands/show(.:format)    brands#show
-#  statics_index GET  /statics/index(.:format)  statics#index
+#        Prefix Verb URI Pattern             Controller#Action
+#          root GET  /                       statics#index
+#       statics GET  /statics(.:format)      statics#index
+#        brands GET  /brands(.:format)       brands#index
+#   brands_show GET  /brands/:id(.:format)   brands#show
+#      products GET  /products(.:format)     products#index
+# products_show GET  /products/:id(.:format) products#show
+
+# what do the parts of the routes mean?
+# 	'/brands/:id is the URL to be matched.
+# 	to: 'brands' refers to the authors_controller.rb
+# 	   #show refers to the show method within brands_controller.rb
+# 	as: 'brands_show' refers to how we are naming our route, it is an alias for the full path /brands/:id
+#      we can use this prefix/alias followed by _path in the erb files to refer to the route
