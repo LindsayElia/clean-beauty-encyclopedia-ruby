8c5c8ff0d7a14ae0397669038db0e610ce3bdd99
LindsayElia <lindsay.elia@gmail.com>
2016-10-02 22:25:53 -0700

    add has_scope gem and add filtering scopes to product model

--- clean_beauty_app/app/controllers/products_controller.rb
+++ clean_beauty_app/app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
+
+	has_scope :by_category_high_level
+	has_scope :by_category_mid_level
+	has_scope :by_category_low_level
+
 	def index
-		@products = Product.order("name").all
+		@products = apply_scopes(Product).all.order("name")
 	end
 
 	def show
--- clean_beauty_app/app/models/product.rb
+++ clean_beauty_app/app/models/product.rb
@@ -14,6 +14,7 @@ class Product < ActiveRecord::Base
 	# order by ingredient name when pulling out the associated data
 	# Rails 4 uses this format of scope blocks, and must be second argument
 
+	# for more readable urls
 	# http://blog.teamtreehouse.com/creating-vanity-urls-in-rails
 	def slug
 		name.downcase.gsub(" ", "-")
@@ -22,5 +23,11 @@ class Product < ActiveRecord::Base
 	def to_param
 		"#{id}-#{slug}"
 	end
+
+	# filtering
+	scope :by_category_high_level, -> category_high_level { where(:category_high_level => category_high_level) }
+	scope :by_category_mid_level, -> category_mid_level { where(:category_mid_level => category_mid_level) }
+	scope :by_category_low_level, -> category_low_level { where(:category_low_level => category_low_level) }
+
 
 end
