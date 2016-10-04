0f209908983613af3d6e164b513f189edc645302
LindsayElia <lindsay.elia@gmail.com>
2016-10-03 19:15:58 -0700

    removing web scrapers folder from app to see if that solves heroku error

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,110 +57,110 @@ end
 # data_ilia = ActiveSupport::JSON.decode(File.read('db/data_from_scraping/ilia.json'))
 
 
-# create products and ingredients, associate both
-count_one_love_organics = 0
-data_one_love_organics.each do |item|
-	# skip item if product exists in the database already, validating for a unique name
-	unless Product.find_by(name: item["name"])
-		# using create! instead of create will tell us if there is a failure creating the record
-		product_one_love_organics = Product.create!(
-			name: item["name"],
-			price_float: (item["price"]).to_f,
-			price_string: item["price"],
-			currency: "USD",
-			size: item["size"],
-		    ingredients_grouping: item["ingredients_grouping"],
-		    image_url: item["image_url"],
-		    image_alt: item["image_alt"],
-		    original_url: item["original_url"]
-			)
-		brand_one_love_organics.products << product_one_love_organics		# associate product with brand
-		count_one_love_organics = count_one_love_organics + 1
+# # create products and ingredients, associate both
+# count_one_love_organics = 0
+# data_one_love_organics.each do |item|
+# 	# skip item if product exists in the database already, validating for a unique name
+# 	unless Product.find_by(name: item["name"])
+# 		# using create! instead of create will tell us if there is a failure creating the record
+# 		product_one_love_organics = Product.create!(
+# 			name: item["name"],
+# 			price_float: (item["price"]).to_f,
+# 			price_string: item["price"],
+# 			currency: "USD",
+# 			size: item["size"],
+# 		    ingredients_grouping: item["ingredients_grouping"],
+# 		    image_url: item["image_url"],
+# 		    image_alt: item["image_alt"],
+# 		    original_url: item["original_url"]
+# 			)
+# 		brand_one_love_organics.products << product_one_love_organics		# associate product with brand
+# 		count_one_love_organics = count_one_love_organics + 1
 
-		ingredients_string = item["ingredients_grouping"]
-		# puts ingredients_string
-		ingredients_array = ingredients_string.split(/\s*,\s*/)
-		# puts ingredients_array
+# 		ingredients_string = item["ingredients_grouping"]
+# 		# puts ingredients_string
+# 		ingredients_array = ingredients_string.split(/\s*,\s*/)
+# 		# puts ingredients_array
 
-		ingredients_array.each do |array_item|
-			# look for ingredient in db, and create if it does not exist
-			ingredient = Ingredient.find_or_create_by!(name: array_item)
-			# associate ingredient & product
-			ingredient.products << product_one_love_organics
-		end
+# 		ingredients_array.each do |array_item|
+# 			# look for ingredient in db, and create if it does not exist
+# 			ingredient = Ingredient.find_or_create_by!(name: array_item)
+# 			# associate ingredient & product
+# 			ingredient.products << product_one_love_organics
+# 		end
 
-	end
-end
-puts "count of one love items added to db:"
-puts count_one_love_organics
-
-
-count_indie_lee = 0
-data_indie_lee.each do |item|
-	unless Product.find_by( name: item["name"])
-		product_indie_lee = Product.create!(
-			name: item["name"],
-			price_float: (item["price"]).to_f,
-			price_string: item["price"],
-			currency: "USD",
-			size: item["size"],
-		    ingredients_grouping: item["ingredients_grouping"],
-		    image_url: item["image_url"],
-		    image_alt: item["image_alt"],
-		    original_url: item["original_url"]
-			)
-		brand_indie_lee.products << product_indie_lee
-		count_indie_lee = count_indie_lee + 1
-
-		ingredients_string = item["ingredients_grouping"]
-		# puts ingredients_string
-		ingredients_array = ingredients_string.split(/\s*,\s*/)
-		# puts ingredients_array
-
-		ingredients_array.each do |array_item|
-			# look for ingredient in db, and create if it does not exist
-			ingredient = Ingredient.find_or_create_by!(name: array_item)
-			# associate ingredient & product
-			ingredient.products << product_indie_lee
-		end
-
-	end
-end
-puts "count of indie lee items added to db:"
-puts count_indie_lee
+# 	end
+# end
+# puts "count of one love items added to db:"
+# puts count_one_love_organics
 
 
-count_ilia = 0
-data_ilia.each do |item|
-	unless Product.find_by( name: item["name"])
-		product_ilia = Product.create!(
-			name: item["name"],
-			price_float: (item["price"]).to_f,
-			price_string: item["price"],
-			currency: "USD",
-			size: item["size"],
-		    ingredients_grouping: item["ingredients_grouping"],
-		    image_url: item["image_url"],
-		    image_alt: item["image_alt"],
-		    original_url: item["original_url"]
-			)
-		brand_ilia.products << product_ilia
-		count_ilia = count_ilia + 1
+# count_indie_lee = 0
+# data_indie_lee.each do |item|
+# 	unless Product.find_by( name: item["name"])
+# 		product_indie_lee = Product.create!(
+# 			name: item["name"],
+# 			price_float: (item["price"]).to_f,
+# 			price_string: item["price"],
+# 			currency: "USD",
+# 			size: item["size"],
+# 		    ingredients_grouping: item["ingredients_grouping"],
+# 		    image_url: item["image_url"],
+# 		    image_alt: item["image_alt"],
+# 		    original_url: item["original_url"]
+# 			)
+# 		brand_indie_lee.products << product_indie_lee
+# 		count_indie_lee = count_indie_lee + 1
 
-		ingredients_string = item["ingredients_grouping"]
-		# puts ingredients_string
-		ingredients_array = ingredients_string.split(/\s*,\s*/)
-		# puts ingredients_array
+# 		ingredients_string = item["ingredients_grouping"]
+# 		# puts ingredients_string
+# 		ingredients_array = ingredients_string.split(/\s*,\s*/)
+# 		# puts ingredients_array
 
-		ingredients_array.each do |array_item|
-			# look for ingredient in db, and create if it does not exist
-			ingredient = Ingredient.find_or_create_by!(name: array_item)
-			# associate ingredient & product
-			ingredient.products << product_ilia
-		end
+# 		ingredients_array.each do |array_item|
+# 			# look for ingredient in db, and create if it does not exist
+# 			ingredient = Ingredient.find_or_create_by!(name: array_item)
+# 			# associate ingredient & product
+# 			ingredient.products << product_indie_lee
+# 		end
 
-	end
-end
-puts "count of ilia items added to db:"
-puts count_ilia
+# 	end
+# end
+# puts "count of indie lee items added to db:"
+# puts count_indie_lee
+
+
+# count_ilia = 0
+# data_ilia.each do |item|
+# 	unless Product.find_by( name: item["name"])
+# 		product_ilia = Product.create!(
+# 			name: item["name"],
+# 			price_float: (item["price"]).to_f,
+# 			price_string: item["price"],
+# 			currency: "USD",
+# 			size: item["size"],
+# 		    ingredients_grouping: item["ingredients_grouping"],
+# 		    image_url: item["image_url"],
+# 		    image_alt: item["image_alt"],
+# 		    original_url: item["original_url"]
+# 			)
+# 		brand_ilia.products << product_ilia
+# 		count_ilia = count_ilia + 1
+
+# 		ingredients_string = item["ingredients_grouping"]
+# 		# puts ingredients_string
+# 		ingredients_array = ingredients_string.split(/\s*,\s*/)
+# 		# puts ingredients_array
+
+# 		ingredients_array.each do |array_item|
+# 			# look for ingredient in db, and create if it does not exist
+# 			ingredient = Ingredient.find_or_create_by!(name: array_item)
+# 			# associate ingredient & product
+# 			ingredient.products << product_ilia
+# 		end
+
+# 	end
+# end
+# puts "count of ilia items added to db:"
+# puts count_ilia
 
